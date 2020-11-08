2b8821a03db2e04a051ed4a1deb6848890875e7a
Ole Jakob Schjøth <ole_jakob94@hotmail.com>
2020-11-08 13:34:41 +0100

    Add project files.

---
+++ NetCoreCICDApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreCICDApplication
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
